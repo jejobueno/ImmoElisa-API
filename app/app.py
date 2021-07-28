6730403fc2e18999525c63f4819088808b8acdd5
jessbuenos@gmail.com <Brussel.20>
2021-07-28 15:10:39 +0200

    'docker file fixed with ports and copy'

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 
 from exceptions.InvalidExpression import InvalidExpression
@@ -35,4 +37,10 @@ def invalid_route(e):
 
 if __name__ == "__main__":
     predictor = Predictor()
-    app.run()
+    # You want to put the value of the env variable PORT if it exist (some services only open specifiques ports)
+    port = int(os.environ.get('PORT', 5000))
+    # Threaded option to enable multiple instances for
+    # multiple user access support
+    # You will also define the host to "0.0.0.0" because localhost will only be reachable from inside de server.
+    app.run(host="0.0.0.0", threaded=True, port=port)
+
